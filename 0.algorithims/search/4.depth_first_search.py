1010cac468f4f82f5a0b31b9fe8b687e56e22c0e
JacobReola42 <jacobreola@gmail.com>
2022-01-23 18:33:37 -0600

    init

---
+++ 0.algorithims/search/4.depth_first_search.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# Copyright 2022 Jacob Reola, all rights reserved
+
+# Uses stack data structure
+def dfs(data, start, visited):
+
+    if start not in visited:
+        print(start, end=" ")
+
+    visited.add(start)  # Add to set
+
+    for i in data[start] - visited:
+        dfs(data, i, visited)   # if not visited recursion.
+    return
+
+__name__ == '__main__'
+visited = set()
+
+# adjacency data in dictionary form
+data = {
+        'A': {'B'},
+        'B': {'A', 'C', 'D'},
+        'C': {'B', 'E'},
+        'D': {'B', 'E'},
+        'E': {'C', 'D', 'F'},
+        'F': {'E'}
+        }
+
+dfs(data, 'D', visited)
