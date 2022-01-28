a71693af8b3637e657bd8b63a2c5070f417e73f6
JacobReola42 <jacobreola@gmail.com>
2022-01-28 08:33:34 -0600

    mv dir

---
+++ 1.stack_queue_heap/0.stack_queue/queue.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# Copyright 2022 Jacob Reola, all rights reserved
+
+from collections import deque
+
+class Queue:
+    
+    def __init__(self):
+       self.queue = deque()
+       self.size = 0
+       self.front = 0
+
+    def __len__(self):
+        return self.size 
+
+    def isEmpty(self):
+        return self.size == 0
+            
+    # append right
+    def enqueue(self, element):
+        self.queue.append(element)
+        self.size += 1
+
+    def peek(self):
+        if self.isEmpty():
+            return("CAN'T PEEK, EMPTY!")
+        return self.queue[self.front]
+
+    # pop left
+    def dequeue(self):
+        if self.isEmpty():
+            return ("CAN'T DEQUEUE, EMPTY!")
+        
+        answer = self.queue[self.front]
+        self.queue[self.front] = None   # helps with garbage collection
+        self.front = (self.front + 1) % len(self.queue)     # Circular semantics
+        self.size -= 1
+        return answer
+
+    def getSize(self):
+        return self.size
