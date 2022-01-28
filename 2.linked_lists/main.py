82d894298eec08a2f92f5327e148f6712b60df9f
JacobReola42 <jacobreola@gmail.com>
2022-01-28 08:22:36 -0600

    updated

--- 2.linked_lists/main.py
+++ 2.linked_lists/main.py
@@ -3,7 +3,10 @@
 
 from singly import LinkedList
 from doubly import DoublyLinkedList
+from circularly import CircularLinkedList
 
+# SINGLY
+print('\nTESTING SINGLY LINKED LIST')
 myList = LinkedList()
 
 print('\nCreating a singly link list.' )
@@ -36,8 +39,8 @@ print('Checking for number 5 in list:', myList.locate(5))
 print('The head of the list is now:', myList.head)
 print()
 
+# DOUBLY
 print('TESTING DOUBLY LINKED LIST\n')
-
 dll = DoublyLinkedList()
 
 print('Creating doubly linked list . . . ')
@@ -51,14 +54,33 @@ dll.print_list()
 print('The list is empty:', dll.isEmpty())
 print('Deleting 44:', dll.remove(44))
 
-
 dll.print_list()
 print("size="+str(dll.size))
 print()
 
+# CIRCULARLY
+print('TESTING CIRCULARLY LINKED LIST\n')
+cll = CircularLinkedList()
 
+for i in [5, 7, 3, 8, 9]:
+    cll.add(i)
 
+print('Test 1.')
+print("size="+str(cll.size))
+print(cll.find(8))
+print(cll.find(12))
 
+my_node = cll.root
+print (my_node, end='->')
 
+for i in range(8):
+    my_node = my_node.nxt_node
+    print (my_node, end='->')
 
-
+print('\n\nTest 2.')
+cll.print_list()
+cll.remove(8)
+print(cll.remove(15))
+print("size="+str(cll.size))
+cll.remove(5)    # delete root node
+cll.print_list()
